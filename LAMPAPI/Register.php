06a2641e03cb0212905bce78c0d2317a25f74890
Ethan Dean <ethandean7@gmail.com>
2024-08-27 16:40:22 -0400

    Remove extra fields from Register Response

--- LAMPAPI/Register.php
+++ LAMPAPI/Register.php
@@ -36,7 +36,7 @@
 	
 	function returnWithError( $err )
 	{
-		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		$retValue = '{"error":"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
 	
