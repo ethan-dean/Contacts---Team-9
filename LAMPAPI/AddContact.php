c90654927aeb47ae1c05dacc3644bba3ca4976d1
SeNiah McField <140975818+nKharisma@users.noreply.github.com>
2024-09-12 22:20:34 -0400

    trying to fix 500 internal sever error code #2

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -1,44 +1,48 @@
 
 <?php
-	//Fully done
-	$inData = getRequestInfo();
-	
-	$userId = $inData["userId"];
-	$firstName = $inData["firstName"];
-	$lastName = $inData["lastName"];
-	$phoneNumber = $inData["phoneNum"];
-	$emailAddress = $inData["emailAddress"];
+$inData = getRequestInfo();
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-	if ($conn->connect_error) 
-	{
-		returnWithError( $conn->connect_error );
-	} 
-	else
-	{
-		$stmt = $conn->prepare("INSERT INTO Contacts (UserId, FirstName, LastName, Phone, Email) VALUES (?,?,?,?,?)");
-		$stmt->bind_param("issss", $userId, $firstName, $lastName, $phoneNumber, $emailAddress);
-		$stmt->execute();
-		$stmt->close();
-		$conn->close();
-		returnWithError("");
-	}
+$userId = $inData["userId"];
+$firstName = $inData["firstName"];
+$lastName = $inData["lastName"];
+$phoneNumber = $inData["phoneNumber"];
+$emailAddress = $inData["emailAddress"];
+$dateCreated = date("Y-m-d H:i:s"); // Current date and time
+$dateLastLoggedIn = date("Y-m-d H:i:s"); // Current date and time
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+$conn = new mysqli("localhost", "TheDatabaseUser", "TheDatabasePassword", "TheDatabaseName");
+if ($conn->connect_error) {
+    returnWithError($conn->connect_error);
+} else {
+    $stmt = $conn->prepare("INSERT INTO Contacts (UserId, FirstName, LastName, Phone, Email, DateCreated, DateLastLoggedIn) VALUES (?,?,?,?,?,?,?)");
+    $stmt->bind_param("issssss", $userId, $firstName, $lastName, $phoneNumber, $emailAddress, $dateCreated, $dateLastLoggedIn);
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
+    if ($stmt->execute() === TRUE) {
+        returnWithInfo("Contact added successfully");
+    } else {
+        returnWithError($stmt->error);
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+
+function getRequestInfo() {
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson($obj) {
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithError($err) {
+    $retValue = '{"error":"' . $err . '"}';
+    sendResultInfoAsJson($retValue);
+}
+
+function returnWithInfo($info) {
+    $retValue = '{"info":"' . $info . '"}';
+    sendResultInfoAsJson($retValue);
+}
 ?>
