40f00f12efc9d7628bd94fff28852f9b0cbd9190
SeNiah McField <140975818+nKharisma@users.noreply.github.com>
2024-09-14 01:10:34 -0400

    reconfigured UpdateContacts.php (testing)

--- LAMPAPI/UpdateContact.php
+++ LAMPAPI/UpdateContact.php
@@ -1,44 +1,43 @@
-
 <?php
-	//Finished 
-	$inData = getRequestInfo();
-	
-	$ID = $inData["ID"];
-	$firstName = $inData["firstName"];
-	$lastName = $inData["lastName"];
-	$phoneNumber = $inData["phoneNum"];
-	$emailAddress = $inData["emailAddress"];
+    $inData = getRequestInfo();
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-	if ($conn->connect_error) 
-	{
-		returnWithError( $conn->connect_error );
-	} 
-	else
-	{
-		$stmt = $conn->prepare("UPDATE Contacts SET FirstName=?,LastName=?,Phone=?,Email=? WHERE ID=?");
-		$stmt->bind_param("sssss", $firstName, $lastName, $phoneNumber, $emailAddress, $ID);
-		$stmt->execute();
-		$stmt->close();
-		$conn->close();
-		returnWithError("");
-	}
+    $id = $inData["ID"];
+    $firstName = $inData["FirstName"];
+    $lastName = $inData["lastName"];
+    $phoneNumber = $inData["phoneNumber"];
+    $emailAddress = $inData["emailAddress"];
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+    if ($conn->connect_error) {
+        returnWithError($conn->connect_error);
+    } else {
+        $stmt = $conn->prepare("UPDATE Contacts SET FirstName=?, LastName=?, Phone=?, Email=? WHERE ID=?");
+        $stmt->bind_param("sssss", $firstName, $lastName, $phoneNumber, $emailAddress, $id);
+        if ($stmt->execute()) {
+            returnWithInfo("Contact updated successfully");
+        } else {
+            returnWithError($stmt->error);
+        }
+        $stmt->close();
+        $conn->close();
+    }
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
+    function getRequestInfo() {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    function sendResultInfoAsJson($obj) {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+
+    function returnWithError($err) {
+        $retValue = '{"error":"' . $err . '"}';
+        sendResultInfoAsJson($retValue);
+    }
+
+    function returnWithInfo($info) {
+        $retValue = '{"info":"' . $info . '"}';
+        sendResultInfoAsJson($retValue);
+    }
 ?>
